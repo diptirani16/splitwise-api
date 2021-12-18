079701d48c8b5f96a5dd46e5cad96a409f60a9fb
Dipti Rani <gdipti642@gmail.com>
2021-12-18 16:31:07 +0530

    done with shares API

--- src/controllers/shareController.js
+++ src/controllers/shareController.js
@@ -1,24 +1,31 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.status(200).send('Happy coding!');
+const shareModel = require('../models/shares');
+
+router.get('/', async (req, res) => {
+    const shares = await shareModel.find().exec();
+    res.status(200).send(shares);
 })
 
-router.get('/:id', (req, res) => {
-    res.status(200).send(`Happy coding: ${req.params.id}`);
+router.get('/:id', async (req, res) => {
+    const share = await shareModel.findById(req.params.id);
+    res.status(200).send(share);
 })
 
-router.post('/', (req, res) => {
-    res.status(201).send(req.body);
+router.post('/', async (req, res) => {
+    const share = await shareModel.create(req.body);
+    res.status(201).send(share);
 })
 
-router.put('/:id', (req, res) => {
-    res.status(200).send(req.body);
+router.put('/:id', async (req, res) => {
+    const share = await shareModel.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).send(share);
 })
 
-router.delete('/:id', (req, res) => {
-    res.status(200).send(req.params.id);
+router.delete('/:id', async (req, res) => {
+    const share = await shareModel.findByIdAndDelete(req.params.id);
+    res.status(200).send(share);
 })
 
 module.exports = router;
